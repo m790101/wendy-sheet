0a9c2c98732b2d1c3b608693cb02e189f7458d73
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2023-11-13 17:56:43 +0800

    新增搜尋功能

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,39 +2,46 @@ import { useEffect, useState } from 'react';
 import Card from '../../component/Card';
 import AddNewModal from './components/AddNewModal';
 import itemApi from '../../api/itemApi';
+import SearchBar from './components/Searchbar';
 
 
 
 const Home = () => {
     const [dataList, setDataList] = useState([])
+    const [searchDataList, setsearchDataList] = useState([])
     const [isVisible, setIsVisible] = useState(false)
     const [isRefresh, setIsRefresh] = useState(false)
 
     if (isRefresh) {
-        getItem(setDataList)
+        getItem(setDataListInitial)
         setIsRefresh(false)
+    }
+    function setDataListInitial(data){
+        setDataList(data)
+        setsearchDataList(data)
     }
 
     useEffect(() => {
-        getItem(setDataList)
+        getItem(setDataListInitial)
     }, [])
 
 
     return (
         <>
+            <SearchBar dataList={dataList} setsearchDataList={setsearchDataList}></SearchBar>
             <AddNewModal
                 isVisible={isVisible}
                 setIsVisible={setIsVisible}
                 setIsRefresh={setIsRefresh}
             ></AddNewModal>
             <div className='d-flex justify-content-center'>
-                <button className='btn btn-success  px-4' onClick={() => { setIsVisible(true) }}>add item</button>
+                <button className='btn btn-success  w-100' onClick={() => { setIsVisible(true) }}>add item</button>
             </div>
 
             <div className='d-flex flex-column'>
                 {
 
-                    dataList.map((item, index) => {
+                    searchDataList.map((item, index) => {
                         return (
                             <div key={index} className='p-3'>
                                 <Card data={item} setIsRefresh={setIsRefresh}></Card>
