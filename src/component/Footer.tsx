60f0b15ece617b33bc4a5ed10d9a3100536f7875
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2023-11-14 18:25:39 +0800

    add footer

---
+++ src/component/Footer.tsx
@@ -0,0 +1,22 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+
+const Footer = () => {
+    return (
+        <div className="footer d-flex justify-content-center align-items-center gap-5">
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
