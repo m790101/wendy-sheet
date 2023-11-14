de77ab3fa92b483b7a1d235d43cc503f6b9d871c
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2023-11-14 18:27:25 +0800

    fix issue

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,5 +1,3 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
-
 const Footer = () => {
     return (
         <div className="footer d-flex justify-content-center align-items-center gap-5">
