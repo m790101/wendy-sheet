cb94a352c2a8e623ad9c89556bd88c8f878799c7
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2023-11-15 15:56:42 +0800

    update new search bar

--- src/pages/Home/components/Searchbar.tsx
+++ src/pages/Home/components/Searchbar.tsx
@@ -1,24 +1,32 @@
-import React from "react";
-import { MouseEvent, SetStateAction, useState } from "react";
+import React, { Dispatch } from "react";
+import { SetStateAction } from "react";
+import { useDispatch } from "react-redux";
+import { AnyAction } from "@reduxjs/toolkit";
+import { updateLoginInfo } from "../../../store/appSlice";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 
 interface NavbarProps {
-    dataList:ItemData[],
-    setSearchDataList:React.Dispatch<SetStateAction<never[]>>
+    dataList: ItemData[],
+    setSearchDataList: React.Dispatch<SetStateAction<never[]>>
 }
 
 
 
-const Navbar = ({ dataList, setSearchDataList }:NavbarProps) => {
-    const [searchText, setSearchText] = useState('')
+const Navbar = ({ dataList, setSearchDataList }: NavbarProps) => {
+    const dispatch = useDispatch()
+
+    const navigate = useNavigate();
 
     return (
         <>
-            <nav className=" position-fixed z-3 navbar">
-                <div className="search_section">
+            <nav className=" position-fixed z-3 navbar d-flex p-3">
+                <div className="search_section gap-4">
                     <div className="d-flex flex-column gap-2">
-                        <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search" onChange={(e) => { setSearchText(e.target.value) }} />
-                        <button className="btn btn-success" type="submit" onClick={(e) => { handleSearch(e, searchText, dataList, setSearchDataList) }}>搜尋</button>
+                        <input className="form-control me-2 " type="search" placeholder="搜尋" aria-label="Search" onChange={(e) => { handleSearch(e,dataList,setSearchDataList) }} />
+                    </div>
+                    <div>
+                        <button className="btn btn-light" onClick={()=>{logout(dispatch,navigate)}}>登出</button>
                     </div>
                 </div>
             </nav>
@@ -29,19 +37,27 @@ const Navbar = ({ dataList, setSearchDataList }:NavbarProps) => {
 
 export default Navbar;
 
-interface ItemData{
-    _id:string,
-    name:string
-    in_stock:number,
-  
-  }
+interface ItemData {
+    _id: string,
+    name: string
+    in_stock: number,
+
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function handleSearch(e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>, searchText: string, dataList: ItemData[], setSearchDataList: (arg0: any) => void) {
+function handleSearch(e: React.ChangeEvent<HTMLInputElement>, dataList: ItemData[], setSearchDataList: (arg0: any) => void) {
     e.preventDefault()
+    const text = e.target.value 
     const filterData = dataList.filter((item) => {
-        return item.name.includes(searchText.trim())
+        return item.name.includes(text.trim())
     })
     setSearchDataList(filterData)
-    
+
 }
+
+function logout(dispatch:Dispatch<AnyAction>,navigate: NavigateFunction){
+    // localStorage.removeItem('token')
+    dispatch(updateLoginInfo({isLogin:false}))
+    navigate('/login')
+}
+
